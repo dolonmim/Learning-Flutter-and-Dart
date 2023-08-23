c287078fa8434d5304bc8097996cca176c6b5c47
Dolon Mim <mimbadhon@gmail.com>
2023-08-23 10:49:11 +0600

    1st Programme

---
+++ 1. Text Style/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromRGBO(103, 58, 183, 1),
+                Color.fromRGBO(55, 13, 127, 1),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello world!',
+              style: TextStyle(
+                color: Color.fromARGB(255, 233, 227, 227),
+                fontSize: 28,
+              ),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
