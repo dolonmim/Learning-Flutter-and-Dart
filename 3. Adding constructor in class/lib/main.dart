9e731fc856a93de9c1bd98da868f43149bf032a9
Dolon Mim <mimbadhon@gmail.com>
2023-08-23 14:38:55 +0600

    full class create

---
+++ 3. Adding constructor in class/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(),
+      ),
+    ),
+  );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromRGBO(103, 58, 183, 1),
+            Color.fromRGBO(55, 13, 127, 1),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello world!',
+          style: TextStyle(
+            color: Color.fromARGB(255, 233, 227, 227),
+            fontSize: 28,
+          ),
+        ),
+      ),
+    );
+  }
+}
